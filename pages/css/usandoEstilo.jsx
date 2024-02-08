d418e1ba9558a6f841b1358820f5796f086168ee
RobertSouDev <rogerhallks1@gmail.com>
2024-02-08 00:33:52 -0300

    aplicando style de forma condicial

--- pages/css/usandoEstilo.jsx
+++ pages/css/usandoEstilo.jsx
@@ -3,10 +3,10 @@ import Estilo from "@/components/estilo"
 export default ()=> {
     return (
         <div>
-            <Estilo numero={3} color="#fff" />
-            <Estilo numero={13} color="red"/>
-            <Estilo numero={-12} />
-
+            <Estilo numero={3} color="#fff" direita />
+            <Estilo numero={-1} color="white"/>
         </div>
     )
 }
+
+// ele vai considera o direita={True} e vai usa esses props
