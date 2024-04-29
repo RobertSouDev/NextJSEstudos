f792926daf4372f3b1a7f62afaf1cddc6f897ee8
RobertSouDev <rogerhallks1@gmail.com>
2024-04-28 23:59:27 -0300

    lista 2

---
+++ data/listaProdutos.js
@@ -0,0 +1,9 @@
+import Produto from "@/mordel/produto";
+
+export default [
+    new Produto(1, "iPad", 7543),
+    new Produto(2, "Monitor 24", 900),
+    new Produto(3, "Computador Game", 6946),
+    new Produto(4, "Samsung S20+", 4946),
+
+]
---
+++ pages/render/repeticao2.jsx
@@ -0,0 +1,39 @@
+import { ErrorBoundaryHandler } from "next/dist/client/components/error-boundary";
+import lista from "../../data/listaProdutos";
+
+export default function repeticao2() {
+    
+    const comBorda = {
+        border: "1px solid #000",
+    }
+
+    function renderizarLinhas() {
+        return lista.map(produto => {
+            return (
+                <tr key={produto.id}>
+                    <td style={comBorda} >{produto.id}</td>
+                    <td style={comBorda} >{produto.nome}</td>
+                    <td style={comBorda} >{produto.preco}</td>
+                </tr>
+            );
+        });
+    }
+
+
+    return (
+        <div>
+            <table style={comBorda}>
+                <thead>
+                    <tr>
+                        <th>Codigo</th>
+                        <th>Nome</th>
+                        <th>Preço</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {renderizarLinhas()}
+                </tbody>
+            </table>
+        </div>
+    );
+}
